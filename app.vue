ed51c438753f7763845dec512899532a13cf3313
joestaxk <joestaxk@gmail.com>
2023-09-22 20:43:13 +0100

    maintenance

--- app.vue
+++ app.vue
@@ -20,8 +20,8 @@
     <Faq />
     <Banner />
     <Footer /> -->
-    <div class="h-[100vh] w-full bg-kcoin text-white sflex justify-center items-center">
-      <h2>Coming soon </h2>
+    <div class="h-[100vh] w-full bg-kcoin text-white flex justify-center items-center">
+      <h2 class="text-3xl font-bold bg-slate-400 p-2 rounded">Maintenance Mode 🛠️</h2>
     </div>
   <!-- </ClientOnly> -->
 </template>
