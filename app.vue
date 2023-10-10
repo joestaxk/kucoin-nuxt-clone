b072313786e9cafac5087db536d2bcf6388068b6
joestaxk <joestaxk@gmail.com>
2023-10-10 12:14:31 +0100

    back online

--- app.vue
+++ app.vue
@@ -11,7 +11,6 @@
 </script>
 
 <template>
-  <!-- <ClientOnly> -->
     <Header />
     <Cover />
     <CryptoMarket />
@@ -23,5 +22,4 @@
     <!-- <div class="h-[100vh] w-full bg-kcoin text-white flex justify-center items-center">
       <h2 class="text-3xl font-bold bg-slate-400 p-2 rounded">Maintenance Mode 🛠️</h2>
     </div> -->
-  <!-- </ClientOnly> -->
 </template>
